d11d6737809cd08b4bed84e15c747d31b899538d
belostok <belostok.fl@gmail.com>
2021-07-29 21:36:18 +0300

    _homepage

---
+++ gulpfile.js
@@ -0,0 +1,91 @@
+const { src, dest, parallel, series, watch } = require('gulp');
+const browserSync = require('browser-sync').create();
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCss = require('gulp-clean-css');
+const rename = require('gulp-rename');
+const imagemin = require('gulp-imagemin');
+const cache = require('gulp-cache');
+const del = require('del');
+
+const distPath = 'dist/';
+
+function scripts() {
+	return src([
+		'./node_modules/swiper/swiper-bundle.min.js'
+	])
+		.pipe(concat('libs.min.js'))
+		.pipe(uglify())
+		.pipe(dest('src/js/'))
+		.pipe(browserSync.stream())
+}
+
+function browsersync() {
+	browserSync.init({
+		server: { baseDir: 'src/' },
+		notify: false,
+		online: true
+	})
+}
+
+function styles() {
+	return src('src/sass/**/*.sass')
+		.pipe(sass())
+		.pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true }))
+		.pipe(cleanCss(({ format: 'beautify' })))
+		.pipe(dest('src/css/'))
+		.pipe(browserSync.stream())
+}
+
+function csslibs() {
+	return src('src/css/libs.css')
+		.pipe(cleanCss(({ level: { 1: { specialComments: 0 } } })))
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(dest('src/css/'))
+}
+
+function images() {
+	return src('src/img/**/*')
+		.pipe(cache(imagemin()))
+		.pipe(dest(`${distPath}img/`))
+}
+
+function clear() {
+	return cache.clearAll();
+}
+
+function clean() {
+	return del([distPath], { force: true });
+}
+
+function startwatch() {
+	watch('src/sass/**/*.sass', styles);
+	watch('src/*.html').on('change', browserSync.reload);
+	watch(['src/**/*.js', '!src/**/*.min.js'], scripts);
+}
+
+function build() {
+	return src([
+		'src/**',
+		'!src/libs',
+		'!src/libs/**',
+		'!src/sass',
+		'!src/sass/**',
+		'!src/css/libs.css',
+		'!src/css/*.map'
+	], { base: 'src/', allowEmpty: true })
+		.pipe(dest(distPath));
+}
+
+exports.browsersync = browsersync;
+exports.scripts = scripts;
+exports.styles = styles;
+exports.csslibs = csslibs;
+exports.images = images;
+exports.clear = clear;
+exports.clean = clean;
+exports.build = series(clean, clear, styles, csslibs, scripts, images, build);
+
+exports.default = parallel(styles, csslibs, scripts, browsersync, startwatch);
---
+++ src/js/common.js
@@ -0,0 +1,183 @@
+const log = console.log
+const isMobile = window.matchMedia('(max-width: 768px)').matches
+
+const classActive = (el, el2) => {
+    if (el.classList.contains('active')) {
+        el.classList.remove('active')
+    } else {
+        el.classList.add('active')
+    }
+    if (el2) {
+        if (el2.classList.contains('active')) {
+            el2.classList.remove('active')
+        } else {
+            el2.classList.add('active')
+        }
+    }
+}
+
+const autoHeight = (content, container) => {
+    if (container.classList.contains('active')) {
+        content.style.height = 0
+    } else {
+        content.style.height = 'auto'
+        const height = content.offsetHeight
+        content.style.height = 0
+        content.style.transition = 'height .3s'
+        setTimeout(() => content.style.height = `${height}px`, 0)
+    }
+}
+
+const emailTest = i =>
+    !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(i.value)
+
+const formAddError = i => {
+    i.parentElement.classList.add('_error')
+    i.classList.add('_error')
+}
+
+const formRemoveError = i => {
+    i.parentElement.classList.remove('_error')
+    i.classList.remove('_error')
+}
+
+const formValidate = f => {
+    let error = 0
+    const req = f.querySelectorAll('._req')
+    for (let i = 0; i < req.length; i++) {
+        const input = req[i]
+        formRemoveError(input)
+        if (input.classList.contains('_email')) {
+            if (emailTest(input)) {
+                formAddError(input)
+                error++
+            }
+        } else if (input.classList.contains('_name')) {
+            if (input.value.length < 2) {
+                formAddError(input)
+                error++
+            }
+        } else if (input.getAttribute('type') === 'checkbox' &&
+            input.checked === false) {
+            formAddError(input)
+            error++
+        }
+    }
+    return error
+}
+
+const formSend = async e => {
+    e.preventDefault()
+    const form = e.target
+    const error = formValidate(form)
+    const formData = new FormData(form)
+
+    if (error === 0) {
+        form.classList.add('_sending')
+        const response = await fetch('../mail.php', {
+            method: 'POST',
+            body: formData
+        })
+        if (response.ok) {
+            // const result = await response.json()
+            log('form sended')
+
+            form.reset()
+            form.classList.remove('_sending')
+        } else {
+            log('sending error')
+            form.classList.remove('_sending')
+        }
+    } else {
+        log('validation error')
+    }
+}
+
+document.addEventListener('scroll', () => {
+
+    const offset = window.scrollY
+    const header = document.querySelector('header')
+    offset > 100 ?
+        header.classList.add('active') :
+        header.classList.remove('active')
+
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const currentYear = document.querySelector('.currentYear')
+    if (currentYear) { currentYear.innerHTML = new Date().getFullYear() }
+
+    const textAngle = document.querySelectorAll('.textAngle')
+    Array.from(textAngle).forEach(e => {
+        const height = e.offsetHeight
+        const compStyles = window.getComputedStyle(e)
+        const lineHeight = compStyles.getPropertyValue('line-height').replace('px', '')
+        const numberOfLines = height / lineHeight
+        const offsetIncrement = 60
+        let highestValue = numberOfLines * offsetIncrement
+        const parentNode = e.parentNode
+        parentNode.style.marginLeft = `-${offsetIncrement}px`
+        for (let i = 0; i <= numberOfLines; i++) {
+            const newSpan = document.createElement('span')
+            newSpan.classList.add('text-offset')
+            newSpan.style.width = `${highestValue}px`
+            newSpan.style.height = `${lineHeight}px` 
+            newSpan.style.float = 'left'
+            newSpan.style.clear = 'left'
+            parentNode.insertBefore(newSpan, e)
+            highestValue = highestValue - offsetIncrement
+        }
+    })
+
+    const clientsSlider = new Swiper('#clientsSlider', {
+        speed: 800,
+        spaceBetween: 20,
+        slidesPerView: 1,
+        loop: true,
+        navigation: {
+            nextEl: '#homeClients .next',
+            prevEl: '#homeClients .prev'
+        }
+    })
+
+    const partnersSlider = new Swiper('#partnersSlider', {
+        speed: 600,
+        spaceBetween: 20,
+        slidesPerView: 5,
+        loop: true,
+        autoplay: {
+            delay: 4000
+        },
+        navigation: {
+            nextEl: '#homePartners .next',
+            prevEl: '#homePartners .prev'
+        }
+    })
+
+    // const callbackForm = document.getElementById('callbackForm')
+    // callbackForm.addEventListener('submit', formSend)
+
+})
+
+document.onreadystatechange = () => {
+    if (document.readyState === 'complete') {
+
+    }
+}
+
+// if (document.getElementById('contactsPage')) {
+//     DG.then(() => {
+//         let map
+//         const mapIcon = DG.icon({
+//             iconUrl: '/assets/app/img/marker.png',
+//             iconSize: isMobile ? [23, 24] : [33, 34]
+//         })
+//         map = DG.map('map', {
+//             center: [55.753466, 37.62017],
+//             zoom: 10
+//         })
+//         DG.marker([55.749874, 37.53774], { icon: mapIcon }).addTo(map)
+//         DG.marker([55.962681, 37.509696], { icon: mapIcon }).addTo(map)
+//     })
+// }
